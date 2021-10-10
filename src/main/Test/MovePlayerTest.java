fb5b7ca4651d1a0f181459e2f7a7f6199e6e563b
ingridstake <ingrid.stake01@gmail.com>
2021-10-10 20:03:50 +0200

    lagt till test för move-metod

---
+++ src/main/Test/MovePlayerTest.java
@@ -0,0 +1,44 @@
+import edu.chalmers.grapefruit.Interfaces.IPositionable;
+import edu.chalmers.grapefruit.Model.GameModel;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MovePlayerTest {
+    private GameModel model;
+
+    @BeforeEach
+    public void testSetUp(){
+        model = new GameModel();
+    }
+
+    @Test
+    public void movePlayerTest() {
+        List<IPositionable> positionables = model.getPositionables();
+        int oldPlayerPositionX, oldPlayerPositionY, newPlayerPositionX, newPlayerPositionY, x = 0, y = 0;
+        int index = 0;
+
+        for (IPositionable positionable : positionables) {
+            if (index == 2) {
+                x = positionable.getX();
+                y = positionable.getY();
+            }
+
+            if (!positionable.getResourceString().equals("node-view.fxml")){
+                oldPlayerPositionX = positionable.getX();
+                oldPlayerPositionY = positionable.getY();
+                model.makePlayerMove(x, y);
+                newPlayerPositionX = positionable.getX();
+                newPlayerPositionY = positionable.getY();
+
+                assert (oldPlayerPositionX != x || oldPlayerPositionY != y);
+                assert (newPlayerPositionX == x && newPlayerPositionY == y);
+                break;
+            }
+            index++;
+        }
+
+    }
+}
