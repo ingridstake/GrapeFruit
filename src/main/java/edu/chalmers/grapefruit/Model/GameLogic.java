d10a87bc5ec7bf02dfe945c5ca42202ba17a1180
Toveenilssonn <tovnil@hotmail.se>
2021-10-18 19:44:58 +0200

    Delat på metoden och lagt till javadoc

--- src/main/java/edu/chalmers/grapefruit/Model/GameLogic.java
+++ src/main/java/edu/chalmers/grapefruit/Model/GameLogic.java
@@ -7,16 +7,15 @@ import edu.chalmers.grapefruit.Model.Position.LogicType;
 import edu.chalmers.grapefruit.Model.Position.TilePosition;
 
 /**
- * @author tovenilsson 
+ * @author tovenilsson
  */
 
 public class GameLogic {
 
     /**
-     * Checks if the tile is unturned, if it is the tile is turned.
-     * Based on the LogicType of the turned tile the current player gets different properties.
-     * @param currentPlayer is the current player
-     * @param newNode is the new position of the player
+     * The gameLogic for "Den försvunna kossan".
+     * @param currentPlayer is the current player.
+     * @param newNode is the new position of the player.
      */
 
     public static void executeGameLogic(IPlayer currentPlayer, Node newNode) {
@@ -24,31 +23,38 @@ public class GameLogic {
         if (position.getLogicType() == LogicType.UNTURNED_TILE) {
             TilePosition tilePosition = position instanceof TilePosition ? (TilePosition) position : null;
             if (tilePosition != null){
-                //TODO flytta ut till en egen metod
-                tilePosition.turnTile();
-
-                switch (tilePosition.getLogicType()){
-                    case COW:
-                        currentPlayer.playerFoundCow();
-                        return;
-                    case COWBELL:
-                        currentPlayer.playerFoundVisa();
-                        return;
-                    case PIG:
-                        currentPlayer.makePigPayment();
-                        return;
-                    case HORSE:
-                        currentPlayer.makeHorsePayment();
-                        return;
-                    case POOP:
-                        currentPlayer.makePoopRobbery();
-                }
+                gameLogicPlayer(currentPlayer, newNode);
             }
-
         }
     }
 
-    private void gameLogicPlayer(){
+    /**
+     * Turns the tile and based on the turned tiles LogicType the player receives different actions.
+     * @param currentPlayer is the current player.
+     * @param newNode is the new position of the player.
+     */
 
+    public static void gameLogicPlayer(IPlayer currentPlayer, Node newNode){
+        IPosition position = newNode.getPosition();
+        TilePosition tilePosition = position instanceof TilePosition ? (TilePosition) position : null;
+
+        tilePosition.turnTile();
+
+        switch (tilePosition.getLogicType()){
+            case COW:
+                currentPlayer.playerFoundCow();
+                return;
+            case COWBELL:
+                currentPlayer.playerFoundVisa();
+                return;
+            case PIG:
+                currentPlayer.makePigPayment();
+                return;
+            case HORSE:
+                currentPlayer.makeHorsePayment();
+                return;
+            case POOP:
+                currentPlayer.makePoopRobbery();
+        }
     }
 }
