62baff2f48954f966524705f9cc8d0693b06fe3f
olimanstrom <olivia@manstrom.se>
2021-10-21 15:14:33 +0200

    players kan inte bli null nu

--- src/main/java/edu/chalmers/grapefruit/Model/GameModel.java
+++ src/main/java/edu/chalmers/grapefruit/Model/GameModel.java
@@ -24,10 +24,10 @@ public class GameModel implements Observable {
      * @param n is the amount of players.
      */
     public void initialize(int n){
-        players = PlayerFactory.MakePlayers(n);
-        if (players == null) {
+        if (n<1 || n>4){
             throw new IllegalArgumentException("More than 4 players is not allowed");
         }
+        players = PlayerFactory.MakePlayers(n);
         gameLogic = GameLogic.createGameLogic(players);
     }
 
